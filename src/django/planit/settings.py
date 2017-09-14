1e21357a8283c9f19c255411db633d22a613e761
Kathryn Killebrew <kkillebrew@azavea.com>
2017-09-14 16:33:08 -0400

    Add custom user model
    Add empty, default custom user model.
    Closes #4.

--- src/django/planit/settings.py
+++ src/django/planit/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -128,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'users.PlanItUser'
 
 # LOGGING CONFIGURATION
 LOGGING = {
