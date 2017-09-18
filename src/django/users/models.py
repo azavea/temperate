5b652176e2f5eb977ff8fe7d3a87162acb7e35da
Kathryn Killebrew <kkillebrew@azavea.com>
2017-09-18 17:02:10 -0400

    Add initial models
    Closes #5.

--- src/django/users/models.py
+++ src/django/users/models.py
@@ -1,5 +1,25 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.gis.db import models
+from django.db.models import CASCADE
+
+from planit_data.models import (CommunitySystem,
+                                Indicator,
+                                WeatherEvent)
 
 
 class PlanItUser(AbstractUser):
     pass
+
+
+class UserLocation(models.Model):
+    geom = models.MultiPolygonField()
+    user = models.ForeignKey(PlanItUser, on_delete=CASCADE, null=False)
+
+
+class UserRisk(models.Model):
+    name = models.CharField(max_length=256, unique=True, blank=False, null=False)
+    notes = models.TextField(null=False, blank=True, default='')
+    location = models.ForeignKey(UserLocation, on_delete=CASCADE, null=False)
+    community_system = models.ForeignKey(CommunitySystem, on_delete=CASCADE, null=False)
+    weather_event = models.ForeignKey(WeatherEvent, on_delete=CASCADE, null=False)
+    indicator = models.ForeignKey(Indicator, on_delete=CASCADE, null=False)
