35f88e5811c99d4e152dad6b1f0fcecb05507388
Michael Maurizi <mmaurizi@azavea.com>
2017-09-22 15:15:32 -0400

    Change user model to use email instead of username
    In order to omit the username field entirely we have to subclass
    `AbstractBaseUser` instead of `AbstractUser`, which also omits several
    other helpful fields, and requires using a custom manager class.
    
    I copied all of the fields (except username) from `AbstractBaseUser`
    to try and keep us as close to the standard Django user as possible.
    
    Fixes #47

--- src/django/users/admin.py
+++ src/django/users/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import PlanItUser
 
+# Need to change a few things in the built-in UserAdmin because we don't have a username field
+UserAdmin.fieldsets = (
+    (None, {'fields': ('email', 'password')}),
+    ('Personal info', {'fields': ('first_name', 'last_name')}),
+    ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                'groups', 'user_permissions')}),
+    ('Important dates', {'fields': ('last_login', 'date_joined')}),
+)
+UserAdmin.add_fieldsets = (
+    (None, {
+        'classes': ('wide',),
+        'fields': ('email', 'password1', 'password2'),
+    }),
+)
+UserAdmin.list_display = ('email', 'first_name', 'last_name', 'is_staff')
+UserAdmin.search_fields = ('first_name', 'last_name', 'email')
+UserAdmin.ordering = ('email',)
+
 admin.site.register(PlanItUser, UserAdmin)
