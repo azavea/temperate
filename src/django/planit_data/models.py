0669aa169c69c51086832012fbe28b47ac16e06f
CloudNiner <andrew@cloudniner.co>
2017-10-04 14:16:26 -0400

    Add __str__ methods for all planit_data models
    So that these objects pretty-print in the
    admin interface and console

--- src/django/planit_data/models.py
+++ src/django/planit_data/models.py
@@ -8,19 +8,31 @@ class GeoRegion(models.Model):
     name = models.CharField(max_length=256, blank=False, null=False)
     geom = models.MultiPolygonField()
 
+    def __str__(self):
+        return self.name
+
 
 class CommunitySystem(models.Model):
     name = models.CharField(max_length=256, unique=True, blank=False, null=False)
 
+    def __str__(self):
+        return self.name
+
 
 class WeatherEvent(models.Model):
     name = models.CharField(max_length=256, unique=True, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Indicator(models.Model):
     name = models.CharField(max_length=256, unique=True, blank=False, null=False)
     label = models.CharField(max_length=512, blank=False, null=False)
     description = models.TextField(blank=True, null=False, default='')
+
+    def __str__(self):
+        return self.name
 
 
 class RiskTemplate(models.Model):
@@ -29,11 +41,17 @@ class RiskTemplate(models.Model):
     indicator = models.ForeignKey(Indicator, on_delete=CASCADE, null=False)
     regions = models.ManyToManyField('GeoRegion', related_name='risk')
 
+    def __str__(self):
+        return '{}, {}, {}'.format(self.community_system, self.weather_event, self.indicator)
+
 
 class UserLocation(models.Model):
     name = models.CharField(max_length=256, blank=False, null=False)
     geom = models.MultiPolygonField()
     user = models.ForeignKey(PlanItUser, on_delete=CASCADE, null=False)
+
+    def __str__(self):
+        return self.name
 
 
 class UserRisk(models.Model):
@@ -43,3 +61,6 @@ class UserRisk(models.Model):
     community_system = models.ForeignKey(CommunitySystem, on_delete=CASCADE, null=False)
     weather_event = models.ForeignKey(WeatherEvent, on_delete=CASCADE, null=False)
     indicator = models.ForeignKey(Indicator, on_delete=CASCADE, null=False)
+
+    def __str__(self):
+        return '{}: {}'.format(self.location.user, self.name)
