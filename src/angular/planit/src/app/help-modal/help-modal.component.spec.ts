b14f2147ca812f209f1305ba864e1fabfd59d1b8
Jenny Fung <jfung@azavea.com>
2017-10-04 16:03:12 -0400

    Fix tests and speed up test runs

--- src/angular/planit/src/app/help-modal/help-modal.component.spec.ts
+++ src/angular/planit/src/app/help-modal/help-modal.component.spec.ts
@@ -1,5 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ModalModule } from 'ngx-bootstrap';
+import { BsModalService } from 'ngx-bootstrap/modal';
+
 import { HelpModalComponent } from './help-modal.component';
 
 describe('HelpModalComponent', () => {
@@ -8,7 +11,9 @@ describe('HelpModalComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HelpModalComponent ]
+      declarations: [ HelpModalComponent ],
+      providers: [ BsModalService ],
+      imports: [ ModalModule.forRoot() ]
     })
     .compileComponents();
   }));
