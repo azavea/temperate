a6b076010a617866e9c6662852cb7cfab976b102
Reed Martz <rmartz@azavea.com>
2017-10-09 14:03:29 -0400

    Clean up view tests

--- src/django/planit_data/tests/test_views.py
+++ src/django/planit_data/tests/test_views.py
@@ -18,14 +18,15 @@ class PlanitApiTestCase(APITestCase):
     def test_concern_list_empty(self):
         response = self.client.get('/api/concern/')
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['results'], [])
 
     def test_concern_list_nonempty(self):
         indicator = Indicator.objects.create()
         Concern.objects.create(indicator=indicator, tagline='test', unit='t', is_relative=True)
         response = self.client.get('/api/concern/')
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_concern_list_nonauth(self):
         """Ensure that unauthenticated users receive a 403 Forbidden reponse."""
@@ -44,12 +45,12 @@ class PlanitApiTestCase(APITestCase):
 
         response = self.client.get('/api/concern/{}'.format(concern.id))
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_concern_detail_invalid(self):
         response = self.client.get('/api/concern/999')
 
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND, msg=response.data)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_concern_detail_nonauth(self):
         client = APIClient()
