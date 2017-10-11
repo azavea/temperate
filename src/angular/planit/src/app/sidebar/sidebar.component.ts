2fdbe91f1b36e7e11e73bd17224464f84e79736e
Jenny Fung <jfung@azavea.com>
2017-10-11 11:46:02 -0400

    Refactor sidebar list

--- src/angular/planit/src/app/sidebar/sidebar.component.ts
+++ src/angular/planit/src/app/sidebar/sidebar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  public tabs = ['Dashboard', 'Indicators', 'Charts', 'Maps', 'Assessment', 'Action Steps', 'Resources']
+
   constructor() { }
 
   ngOnInit() {
