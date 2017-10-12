ceffa79a294df9b8b37d2a3841c66f7bb1ac5a9a
Jenny Fung <jfung@azavea.com>
2017-10-12 15:00:02 -0400

    Complete working draft of refresh token mgmt cmd

--- src/django/planit_data/management/commands/set_token.py
+++ src/django/planit_data/management/commands/set_token.py
@@ -4,48 +4,53 @@ import boto3
 import os
 import requests
 import json
+import re
 
 from django.core.management.base import BaseCommand
+
+from planit_data.models import APIToken
 
 logger = logging.getLogger(__name__)
 
 TFVARS_BUCKET = 'staging-us-east-1-climate-config'
 TFVARS_KEY = 'terraform/planit/terraform.tfvars'
+REGEX_EMAIL = 'CCAPI_email = "(.*?)\"'
+REGEX_PASSWORD = 'CCAPI_password = "(.*?)\"'
 
 
 class Command(BaseCommand):
+    """Requires connection to the Azavea IP."""
 
     help = 'refreshes and saves a CC API token'
 
     def handle(self, *args, **options):
         client = boto3.client('s3')
-        import ipdb;ipdb.set_trace()
-        response = client.get_object(Bucket=TFVARS_BUCKET,Key=TFVARS_KEY)
+        response = client.get_object(Bucket=TFVARS_BUCKET, Key=TFVARS_KEY)
         byte_data = response['Body'].read()
-        #json_data = json.loads(byte_data.decode(encoding='utf-8'))
+
+        match = re.search(REGEX_EMAIL, str(byte_data))
+        email = match.groups(1)[0]
+
+        match = re.search(REGEX_PASSWORD, str(byte_data))
+        password = match.groups(1)[0]
 
         environment = os.getenv('DJANGO_ENV')
-        if environment == 'development':
-            URL = 'https://app.staging.climate.azavea.com'#'http://localhost:8080'
-        elif environment == 'production':
-            URL = 'https://app.climate.azavea.com'
-        elif environment == 'staging':
-            URL = 'https://app.staging.climate.azavea.com'
-        else:
-            logger.info('Did not update API token')
-            return
+        if environment == 'production':
+            url = 'https://app.climate.azavea.com'
+        elif environment == 'development' or environment == 'staging':
+            url = 'https://app.staging.climate.azavea.com'
 
-        print (environment, URL)
-
+        # Change to personal info to test against your account
         data = [
-            ('email', 'jfung@azavea.com'),
-            ('password', 'adminadmin'),
+            ('email', email),  # test@azavea.com
+            ('password', password),  # testtest
         ]
-        # Get token
-        request = requests.post(URL + '/api-token-auth/', data=data, verify=False)
-        current_token = json.loads(request.text)['token']
 
-        header = {'Authorization': 'Token ' + current_token}
-        request2 = requests.post(URL + '/accounts/api/new_token/', headers=header, verify=False)
-        import ipdb;ipdb.set_trace()
-        print (request2)
+        # Get and set new token
+        request = requests.post(url + '/api-refresh-token-auth/', data=data, verify=False)
+        if request.status_code == 200:
+            new_token = json.loads(request.text)['token']
+            APIToken.objects.all().delete()
+            APIToken.objects.create(token=new_token)
+        else:
+            print('Error refreshing token. {}: {}'.format(request.status_code, request.reason))
