975f9d3f5998fd7997c0f471005c039cccd2ba38
Jenny Fung <jfung@azavea.com>
2017-10-16 15:38:41 -0400

    Reorganize components into doc-recommended modules

--- src/angular/planit/src/app/app.module.ts
+++ src/angular/planit/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { environment } from '../environments/environment';
 
@@ -11,12 +9,13 @@ import { MarketingComponent } from './marketing/marketing.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { IndicatorsComponent } from './indicators/indicators.component';
 import { PageNotFoundComponent } from './not-found.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { UserDropdownComponent } from './user-dropdown/user-dropdown.component';
 
+import { CoreModule } from './core/core.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { IndicatorsModule } from './indicators/indicators.module';
+import { SharedModule } from './shared/shared.module';
 
 import { ModalModule } from 'ngx-bootstrap';
-import { HelpModalComponent } from './help-modal/help-modal.component';
 
 const appRoutes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
@@ -29,25 +28,24 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     MarketingComponent,
-    DashboardComponent,
-    IndicatorsComponent,
-    HelpModalComponent,
-    PageNotFoundComponent,
-    SidebarComponent,
-    UserDropdownComponent
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    CoreModule,
+    DashboardModule,
+    IndicatorsModule,
     ModalModule.forRoot(),
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: !environment.production } // <-- debugging purposes only
     ),
-    BsDropdownModule.forRoot(),
+    SharedModule
   ],
   exports: [
     RouterModule
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
