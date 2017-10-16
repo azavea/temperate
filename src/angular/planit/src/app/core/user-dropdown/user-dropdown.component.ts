81c47cb8a59570c712d6ce91167498890a6e9168
Jenny Fung <jfung@azavea.com>
2017-10-16 16:01:13 -0400

    Rehome models

--- src/angular/planit/src/app/core/user-dropdown/user-dropdown.component.ts
+++ src/angular/planit/src/app/core/user-dropdown/user-dropdown.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 
-import { User } from '../../models/user.model';
+import { User } from '../../shared/models/user.model';
 
 @Component({
   selector: 'app-user-dropdown',
