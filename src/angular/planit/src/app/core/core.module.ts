30b259949466dbdccf6bb3e3bf53604dbfdd97bd
Jenny Fung <jfung@azavea.com>
2017-10-19 12:51:00 -0400

    Rehouse navbar in shared module

--- src/angular/planit/src/app/core/core.module.ts
+++ src/angular/planit/src/app/core/core.module.ts
@@ -1,25 +1,13 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HelpModalComponent } from './help-modal/help-modal.component';
-import { UserDropdownComponent } from './user-dropdown/user-dropdown.component';
-
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { NavbarComponent } from './navbar/navbar.component';
-
 @NgModule({
   imports: [
     CommonModule,
-    BsDropdownModule.forRoot(),
   ],
   declarations: [
-    HelpModalComponent,
-    UserDropdownComponent,
-    NavbarComponent
   ],
   exports: [
-    HelpModalComponent,
-    UserDropdownComponent
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
--- src/angular/planit/src/app/shared/shared.module.ts
+++ src/angular/planit/src/app/shared/shared.module.ts
@@ -4,21 +4,33 @@ import { RouterModule } from '@angular/router';
 
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { TopConcernsComponent } from './top-concerns/top-concerns.component';
+import { HelpModalComponent } from './help-modal/help-modal.component';
+import { UserDropdownComponent } from './user-dropdown/user-dropdown.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    BsDropdownModule.forRoot()
   ],
   declarations: [
     SidebarComponent,
     TopConcernsComponent
+    HelpModalComponent,
+    UserDropdownComponent,
+    NavbarComponent
   ],
   exports: [
+    HelpModalComponent,
+    UserDropdownComponent,
     CommonModule,
     RouterModule,
     SidebarComponent,
     TopConcernsComponent
+    NavbarComponent
   ]
 })
 export class SharedModule { }
