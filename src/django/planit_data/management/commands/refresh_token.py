f4995fe1652d0294dda80c208a00fa582b8066b7
Jenny Fung <jfung@azavea.com>
2017-10-19 11:18:09 -0400

    Update token refresh URL and rebase

--- src/django/planit_data/management/commands/refresh_token.py
+++ src/django/planit_data/management/commands/refresh_token.py
@@ -34,20 +34,19 @@ class Command(BaseCommand):
         match = re.search(REGEX_PASSWORD, str(byte_data))
         password = match.groups(1)[0]
 
-        environment = os.getenv('DJANGO_ENV')
+        environment = os.getenv('DJANGO_ENV').lower()
         if environment == 'production':
             url = 'https://app.climate.azavea.com'
         elif environment == 'development' or environment == 'staging':
             url = 'https://app.staging.climate.azavea.com'
 
-        # Change to personal info to test against your account
         data = [
-            ('email', email),  # test@azavea.com
-            ('password', password),  # testtest
+            ('email', email),
+            ('password', password),
         ]
 
         # Get and set new token
-        request = requests.post(url + '/api-refresh-token-auth/', data=data, verify=False)
+        request = requests.post(url + '/api-token-auth/refresh/', data=data, verify=False)
         if request.status_code == 200:
             new_token = json.loads(request.text)['token']
             APIToken.objects.all().delete()
--- src/django/planit_data/migrations/0006_apitoken.py
+++ src/django/planit_data/migrations/0006_apitoken.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('planit_data', '0003_add_name_to_georegion_userlocation'),
+        ('planit_data', '0005_auto_20171010_1644'),
     ]
 
     operations = [
