8ac6fa6a8d9ec9fbbbbdd59fdbb9a97e663291a8
Jenny Fung <jfung@azavea.com>
2017-10-24 12:47:40 -0400

    Set up django to get CCAPI creds from environment

--- src/django/planit_data/management/commands/refresh_token.py
+++ src/django/planit_data/management/commands/refresh_token.py
@@ -1,48 +1,30 @@
 import logging
 
-import boto3
-import os
 import requests
 import json
-import re
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from planit_data.models import APIToken
 
 logger = logging.getLogger(__name__)
 
-TFVARS_BUCKET = 'staging-us-east-1-climate-config'
-TFVARS_KEY = 'terraform/planit/terraform.tfvars'
-REGEX_EMAIL = 'CCAPI_email = "(.*?)\"'
-REGEX_PASSWORD = 'CCAPI_password = "(.*?)\"'
-
 
 class Command(BaseCommand):
-    """Requires connection to the Azavea IP."""
 
     help = 'refreshes and saves a CC API token'
 
     def handle(self, *args, **options):
-        client = boto3.client('s3')
-        response = client.get_object(Bucket=TFVARS_BUCKET, Key=TFVARS_KEY)
-        byte_data = response['Body'].read()
-
-        match = re.search(REGEX_EMAIL, str(byte_data))
-        email = match.groups(1)[0]
-
-        match = re.search(REGEX_PASSWORD, str(byte_data))
-        password = match.groups(1)[0]
-
-        environment = os.getenv('DJANGO_ENV').lower()
+        environment = settings.ENVIRONMENT.lower()
         if environment == 'production':
             url = 'https://app.climate.azavea.com'
         elif environment == 'development' or environment == 'staging':
             url = 'https://app.staging.climate.azavea.com'
 
         data = [
-            ('email', email),
-            ('password', password),
+            ('email', settings.CCAPI_EMAIL),
+            ('password', settings.CCAPI_PASSWORD),
         ]
 
         # Get and set new token
@@ -51,5 +33,7 @@ class Command(BaseCommand):
             new_token = json.loads(request.text)['token']
             APIToken.objects.all().delete()
             APIToken.objects.create(token=new_token)
+            logger.debug('Token is now {}'.format(APIToken.objects.all()[0].token))
         else:
-            print('Error refreshing token. {}: {}'.format(request.status_code, request.reason))
+            logger.debug('Error refreshing token. {}: {}'.format(request.status_code,
+                                                                 request.reason))
