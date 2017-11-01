e57e1a5063f8eef7f292ae07d7b41543c5817e26
Kathryn Killebrew <kkillebrew@azavea.com>
2017-11-01 14:23:59 -0400

    Change host reference
    Note outstanding PR to set host.

--- src/angular/planit/src/app/shared/services/account-create.service.ts
+++ src/angular/planit/src/app/shared/services/account-create.service.ts
@@ -4,8 +4,8 @@ import { Observable } from 'rxjs/Rx';
 
 import { User } from '../models/user.model';
 
-// TODO: add
-//import { apiHost } from '../constants';
+// TODO: add when #159 done
+//import { environment } from '../../../environments/environment';
 
 /*
  * Account Creation Service
@@ -17,10 +17,11 @@ export class AccountCreateService {
     constructor(private http: Http) {}
 
     public create(user: User): Observable<User> {
-      // TODO: import
-      const apiHost = 'http://localhost:8100';
 
-      const url = `${apiHost}/api/users/`;
+      // TODO: import when #159 done
+      const environment = {apiUrl: 'http://localhost:8100'};
+
+      const url = `${environment.apiUrl}/api/users/`;
       return this.http.post(url, user)
         .map(resp => resp.json() || {} as User)
         .catch((error: Response) => {
