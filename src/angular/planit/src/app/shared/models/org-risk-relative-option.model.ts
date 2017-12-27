0bff91e8f860d1f1cd7ad36fa0f09cb6abd6fee0
Kathryn Killebrew <kkillebrew@azavea.com>
2017-12-27 15:32:43 -0500

    Add keys and values to enum
    Add convenience functions to get keys and values from the risk option enum.
    Typescript does not provide these built-in.

--- src/angular/planit/src/app/shared/models/org-risk-relative-option.model.ts
+++ src/angular/planit/src/app/shared/models/org-risk-relative-option.model.ts
@@ -1,3 +1,4 @@
+// The adaptive need tooltip box expects these to be declared in ascending severity
 export enum OrgRiskRelativeOption {
   Unsure = 'unsure',
   Low = 'low',
@@ -5,4 +6,24 @@ export enum OrgRiskRelativeOption {
   Moderate = 'moderate',
   ModeratelyHigh = 'mod high',
   High = 'high'
+}
+
+export namespace OrgRiskRelativeOption {
+  // get the enum keys and values. see:
+  // https://stackoverflow.com/a/39813213/8202149
+  export function keys(): string[] {
+    var k = Object.keys(OrgRiskRelativeOption);
+    console.log(k);
+    return k.slice(0, k.length - 2); // last values are the strings "keys" and "values" for some reason
+  }
+
+  export function values(): string[] {
+    var vals: string[] = [];
+    for (const val in OrgRiskRelativeOption) {
+      if (typeof(OrgRiskRelativeOption[val]) === 'string') {
+        vals.push(OrgRiskRelativeOption[val]);
+      }
+    }
+    return vals;
+  }
 }
