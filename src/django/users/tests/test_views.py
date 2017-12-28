234f3268b0d4e689a2d75140048317f5dbe303d7
Reed Martz <rmartz@azavea.com>
2017-12-28 10:46:11 -0500

    Avoid CSRF token requirement if API token is provided.

--- src/django/users/tests/test_views.py
+++ src/django/users/tests/test_views.py
@@ -137,7 +137,7 @@ class UserCreationApiTestCase(APITestCase):
     def test_get_auth_required(self):
         """Ensure an unauthenticated request cannot GET."""
         response = self.client.get('/api/users/', format='json')
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class UserAuthenticationApiTestCase(TestCase):
@@ -301,3 +301,98 @@ class OrganizationApiTestCase(APITestCase):
         # The organization should not have been changed
         org.refresh_from_db()
         self.assertEqual(org.name, "Starting Name")
+
+    def test_org_delete_success(self):
+        org = PlanItOrganization.objects.create(name='Test Organization')
+
+        url = reverse('planitorganization-detail', kwargs={'pk': org.id})
+        result = self.client.delete(url)
+
+        self.assertEqual(result.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(PlanItOrganization.objects.filter(id=org.id).exists())
+
+
+class CsrfTestCase(TestCase):
+    def setUp(self):
+        # Create a client that enforces CSRF checks
+        self.client = Client(enforce_csrf_checks=True)
+
+    def test_csrf_for_token_auth(self):
+        """Ensure that CSRF token is not needed when API token is used.
+
+        The Temperate does not use session authentication, and uses API tokens instead of a CSRF
+        token (Which is needed to prevent a user's session from being hijacked). A user that is
+        session authenticated in the background should be able to use unsafe methods so long as
+        their API token is provided.
+        """
+        user = PlanItUser.objects.create_user(
+            first_name='Test',
+            last_name='User',
+            email='user@azavea.com',
+            password='testpass'
+        )
+        token = Token.objects.get(user=user)
+
+        # Log in with the client using a non-token authentication (e.g. session)
+        self.client.force_login(user=user)
+
+        # Configure test data
+        org = PlanItOrganization.objects.create(name='Test Organization')
+
+        # Make a request that doesn't include CSRF token, but does include API token
+        url = reverse('planitorganization-detail', kwargs={'pk': org.id})
+        result = self.client.delete(
+            url,
+            HTTP_AUTHORIZATION='Token {}'.format(token.key)
+        )
+
+        # Ensure the request was accepted
+        self.assertTrue(status.is_success(result.status_code),
+                        "Expected success status, received {} instead".format(result.status_code))
+
+    def test_csrf_hijack_without_token_auth(self):
+        """Ensure that requests without a CSRF or API token are rejected."""
+        user = PlanItUser.objects.create_user(
+            first_name='Test',
+            last_name='User',
+            email='user@azavea.com',
+            password='testpass'
+        )
+
+        # Log in with the client using a non-token authentication (e.g. session)
+        self.client.force_login(user=user)
+
+        # Configure test data
+        org = PlanItOrganization.objects.create(name='Test Organization')
+
+        # Make a request that doesn't include a CSRF or API token, indicating a potential hijack
+        url = reverse('planitorganization-detail', kwargs={'pk': org.id})
+        result = self.client.delete(url)
+
+        # Ensure the request was accepted
+        self.assertTrue(status.is_client_error(result.status_code),
+                        "Expected error status, received {} instead".format(result.status_code))
+
+    def test_basic_token_auth(self):
+        """Ensure that API token authentication enables a user to perform actions."""
+        user = PlanItUser.objects.create_user(
+            first_name='Test',
+            last_name='User',
+            email='user@azavea.com',
+            password='testpass'
+        )
+        token = Token.objects.get(user=user)
+
+        # Configure test data
+        org = PlanItOrganization.objects.create(name='Test Organization')
+
+        # Make a request explicitly using token authentication
+        url = reverse('planitorganization-detail', kwargs={'pk': org.id})
+        result = self.client.delete(
+            url,
+            HTTP_AUTHORIZATION='Token {}'.format(token.key)
+        )
+
+        # Ensure the request was accepted
+        self.assertTrue(status.is_success(result.status_code),
+                        "Expected success status, received {} instead".format(result.status_code))
