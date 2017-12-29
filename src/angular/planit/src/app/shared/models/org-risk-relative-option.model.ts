69f0e134174ade2813040d527bfa5704e9753bb4
Kathryn Killebrew <kkillebrew@azavea.com>
2017-12-29 12:53:20 -0500

    Remove unused namespace

--- src/angular/planit/src/app/shared/models/org-risk-relative-option.model.ts
+++ src/angular/planit/src/app/shared/models/org-risk-relative-option.model.ts
@@ -7,23 +7,3 @@ export enum OrgRiskRelativeOption {
   ModeratelyHigh = 'mod high',
   High = 'high'
 }
-
-export namespace OrgRiskRelativeOption {
-  // get the enum keys and values. see:
-  // https://stackoverflow.com/a/39813213/8202149
-  export function keys(): string[] {
-    const k = Object.keys(OrgRiskRelativeOption);
-    // last two values are the strings "keys" and "values", the function names
-    return k.slice(0, k.length - 2);
-  }
-
-  export function values(): string[] {
-    const vals: string[] = [];
-    for (const val in OrgRiskRelativeOption) {
-      if (typeof(OrgRiskRelativeOption[val]) === 'string') {
-        vals.push(OrgRiskRelativeOption[val]);
-      }
-    }
-    return vals;
-  }
-}
