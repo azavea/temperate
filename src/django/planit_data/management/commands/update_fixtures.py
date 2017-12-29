3e0da788f52005c4c55391b9ce1b2f4bf0b21c3a
Reed Martz <rmartz@azavea.com>
2017-12-29 13:47:45 -0500

    Add action_steps fixtures to definitions

--- src/django/planit_data/management/commands/update_fixtures.py
+++ src/django/planit_data/management/commands/update_fixtures.py
@@ -4,29 +4,36 @@ from django.core.management import call_command
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
-FIXTURES = [
-    # Organized (filename, model_list)
-    ('communitysystems.json', ['planit_data.communitysystem']),
-    ('concerns.json', ['planit_data.concern']),
-    ('defaultrisks.json', ['planit_data.defaultrisk']),
-    ('georegions.json', ['planit_data.georegion']),
-    ('indicators.json', ['planit_data.indicator']),
-    ('weathereventrank.json', ['planit_data.weathereventrank']),
-    ('weatherevents.json', ['planit_data.weatherevent'])
-]
+FIXTURES = {
+    'planit_data': [
+        # Organized (filename, model_list)
+        ('communitysystems.json', ['planit_data.communitysystem']),
+        ('concerns.json', ['planit_data.concern']),
+        ('defaultrisks.json', ['planit_data.defaultrisk']),
+        ('georegions.json', ['planit_data.georegion']),
+        ('indicators.json', ['planit_data.indicator']),
+        ('weathereventrank.json', ['planit_data.weathereventrank']),
+        ('weatherevents.json', ['planit_data.weatherevent']),
+    ],
+    'action_steps': [
+        ('actioncategories.json', ['action_steps.actioncategory']),
+        ('collaborators.json', ['action_steps.collaborator']),
+    ]
+}
 
 
 class Command(BaseCommand):
     help = 'dumps base data to fixtures'
 
     def handle(self, *args, **options):
-        for filename, model_list in FIXTURES:
-            output_path = os.path.join(settings.BASE_DIR, 'planit_data', 'fixtures', filename)
-            call_command(
-                'dumpdata',
-                *model_list,
-                output=output_path,
-                indent=4,
-                natural_foreign=True,
-                natural_primary=True
-            )
+        for project, output_fixtures in FIXTURES.items():
+            for filename, model_list in output_fixtures:
+                output_path = os.path.join(settings.BASE_DIR, project, 'fixtures', filename)
+                call_command(
+                    'dumpdata',
+                    *model_list,
+                    output=output_path,
+                    indent=4,
+                    natural_foreign=True,
+                    natural_primary=True
+                )
