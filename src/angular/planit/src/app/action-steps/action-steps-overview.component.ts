dab243314106943d0c1d37b5282bdee678226553
Kathryn Killebrew <kkillebrew@azavea.com>
2018-01-05 11:58:30 -0500

    Check whether risks assessed

--- src/angular/planit/src/app/action-steps/action-steps-overview.component.ts
+++ src/angular/planit/src/app/action-steps/action-steps-overview.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { RiskService } from '../core/services/risk.service';
 import { Risk } from '../shared';
+import { RiskService } from '../core/services/risk.service';
 
 @Component({
   selector: 'as-overview',
@@ -11,11 +11,29 @@ import { Risk } from '../shared';
 export class ActionStepsOverviewComponent implements OnInit {
 
   public risks: Risk[];
+  public haveAssessedRisks: boolean = false;
 
-  constructor(private riskService: RiskService) { }
+  constructor (private riskService: RiskService) {}
 
   ngOnInit() {
-    this.riskService.list().subscribe(risks => this.risks = risks);
+    this.riskService.list().subscribe(risks => {
+      this.risks = risks;
+      // now that risks have been fetched, check if any have been assessed
+      checkAssessedRisks();
+    });
+  }
+
+  // Check if any of the risks have been assessed yet
+  checkAssessedRisks() {
+    for (let risk: Risk in this.risks) {
+      if (risk.potentialImpact || risk.adaptiveCapacity) {
+        this.haveAssessedRisks = true;
+        return;
+      }
+    }
+
+    // if got this far, no risks found with assessment values set
+    this.haveAssessedRisks = false;
   }
 
 }
