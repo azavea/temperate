263676024827361391ad28481b49b7bd9089ad7a
Sharp Hall <shall@azavea.com>
2018-01-09 16:09:40 -0500

    Add review step to new risk wizard

--- src/angular/planit/src/app/risk-wizard/steps/review-step/review-step.component.ts
+++ src/angular/planit/src/app/risk-wizard/steps/review-step/review-step.component.ts
@@ -1,19 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+
+import { Risk, OrgRiskRelativeOption, OrgRiskRelativeChanceOptions,
+    WizardStepComponent } from '../../../shared/';
+import { RiskStepKey } from '../../risk-step-key';
+
+import { WizardSessionService } from '../../../core/services/wizard-session.service';
+
+export interface ReviewStepFormModel {
+}
 
 @Component({
   selector: 'app-risk-step-review',
   templateUrl: 'review-step.component.html'
 })
 
-export class ReviewStepComponent implements OnInit {
+export class ReviewStepComponent extends WizardStepComponent<Risk, ReviewStepFormModel> implements OnInit {
 
   public navigationSymbol = '5';
   public title = 'Review';
 
-  constructor(private router: Router) { }
+  public form: FormGroup;
+  public risk: Risk;
+  public key: RiskStepKey = RiskStepKey.Review;
 
-  ngOnInit() { }
+  constructor(private router: Router,
+              protected session: WizardSessionService<Risk>) {
+    super(session);
+  }
+
+  ngOnInit() {
+    super.ngOnInit();
+    this.risk = this.session.getData();
+  }
+
+  getFormModel(): ReviewStepFormModel {
+    const data: ReviewStepFormModel = {};
+    return data;
+  }
+
+  fromModel(model: Risk): ReviewStepFormModel {
+    return {
+    };
+  }
+
+  toModel(data: ReviewStepFormModel, model: Risk) {
+    return model;
+  }
+
+  setupForm(data: ReviewStepFormModel) {
+  }
 
   finish() {
     this.router.navigate(['assessment']);
