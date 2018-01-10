5bc5105c52a7c588a9dce903432392576b1f0eb5
Kathryn Killebrew <kkillebrew@azavea.com>
2018-01-10 11:10:49 -0500

    Implement wizard methods for category step

--- src/angular/planit/src/app/action-wizard/steps/category-step/category-step.component.ts
+++ src/angular/planit/src/app/action-wizard/steps/category-step/category-step.component.ts
@@ -1,43 +1,89 @@
 import { Component, OnInit } from '@angular/core';
 
-import { ActionCategory } from '../../../shared/';
+import { Action, ActionCategory, WizardStepComponent } from '../../../shared/';
 import { ActionCategoryService } from '../../../core/services/action-category.service';
+import { ActionStepKey } from '../../action-step-key';
+import { WizardSessionService } from '../../../core/services/wizard-session.service';
 
 @Component({
   selector: 'app-action-category-step',
   templateUrl: './category-step.component.html'
 })
-export class CategoryStepComponent implements OnInit {
+export class CategoryStepComponent extends WizardStepComponent<Action, ActionCategory[]>
+                                   implements OnInit {
 
   public navigationSymbol = '4';
   public title = 'Categories';
+  public form = null; // no form, just buttons
+  public key = ActionStepKey.Category;
 
+  public action: Action;
   public actionCategories: ActionCategory[];
 
-  constructor(private actionCategoryService: ActionCategoryService) { }
+  constructor(private actionCategoryService: ActionCategoryService,
+              protected session: WizardSessionService<Action>) {
+    super(session);
+  }
 
   ngOnInit() {
-    this.actionCategoryService.list().subscribe(categories => this.actionCategories = categories);
+    super.ngOnInit();
+    this.action = this.session.getData() || new Action({});
+
+    this.actionCategoryService.list().subscribe(categories => {
+      this.actionCategories = categories;
+      this.setupForm(this.fromModel(this.action));
+    });
   }
 
   // check if any of the action categories have been selected
   public haveSelectedActionCategory(): boolean {
-    if (!this.actionCategories) {
-      return false; // when not fully initialized, component has no categories yet
-    }
-
-    for (const category of this.actionCategories) {
-      if (category.selected) {
-        return true;
-      }
-    }
-
-    return false; // none selected
+    return this.actionCategories && !!this.actionCategories.find(function(cat: ActionCategory) {
+      return cat.selected;
+    });
   }
 
   // toggle button selections on click
   public selectActionCategory(actionCategory: ActionCategory) {
     actionCategory.selected = !actionCategory.selected;
+  }
+
+  fromModel(model: Action): ActionCategory[] {
+    return model.categories;
+  }
+
+  getFormModel(): ActionCategory[] {
+    return this.actionCategories.filter(function(cat: ActionCategory) {
+      return cat.selected;
+    });
+  }
+
+  setupForm(data: ActionCategory[]) {
+    if (!this.actionCategories) {
+      console.warn('Cannot set up category step form until action categories loaded');
+      return;
+    }
+
+    if (!data) {
+      return; // no action categories set on action model yet
+    }
+
+    // mark action categories from the Action model as `selected` for UI presentation
+    for (const category of data) {
+      const match: ActionCategory = this.actionCategories.find(function(cat: ActionCategory) {
+        return cat.id === category.id;
+      });
+
+      if (match) {
+        match.selected = true;
+      } else {
+        console.warn('Action model has unrecognized category set: ' + category.name);
+      }
+    }
+  }
+
+  toModel(data: ActionCategory[], model: Action): Action {
+    model.categories = data;
+    return model;
   }
 
 }
