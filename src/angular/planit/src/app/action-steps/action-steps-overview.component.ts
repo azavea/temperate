f6e81c918cedf54936f6cde6d0b2a1f290d9e86f
Kathryn Killebrew <kkillebrew@azavea.com>
2018-01-10 14:07:51 -0500

    Update type for risk properties for progress bar
    Type of properites on Risk have changed to options; update to reflect that.

--- src/angular/planit/src/app/action-steps/action-steps-overview.component.ts
+++ src/angular/planit/src/app/action-steps/action-steps-overview.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Risk } from '../shared';
+import { OrgRiskRelativeOption, Risk } from '../shared';
 import { RiskService } from '../core/services/risk.service';
 
 @Component({
@@ -20,7 +20,7 @@ export class ActionStepsOverviewComponent implements OnInit {
     this.riskService.list().subscribe(risks => {
       this.risks = risks;
       // now that risks have been fetched, check if any have been assessed
-      this.checkAssessedRisks();
+      this.haveAssessedRisks = this.checkAssessedRisks();
       // get count of risks with actions
       this.getRisksWithActionsCount();
     });
@@ -28,27 +28,16 @@ export class ActionStepsOverviewComponent implements OnInit {
 
   // Check if any of the risks have been assessed yet
   // TODO: #405 implement another method/property for checking if assessed or not
-  checkAssessedRisks() {
-    for (const risk of this.risks) {
-      if (risk.impactMagnitude || risk.adaptiveCapacity) {
-        this.haveAssessedRisks = true;
-        return;
-      }
-    }
-
-    // if got this far, no risks found with assessment values set
-    this.haveAssessedRisks = false;
+  checkAssessedRisks(): boolean {
+    return !!this.risks.find((risk: Risk) => {
+      return risk.impactMagnitude !== OrgRiskRelativeOption.Unsure ||
+        risk.adaptiveCapacity !== OrgRiskRelativeOption.Unsure;
+    });
   }
 
   // Count how many risks have associated actions, for the progress bar
   getRisksWithActionsCount() {
-    this.risksWithActionsCount = 0;
-
-    for (const risk of this.risks) {
-      if (risk.relatedAdaptiveValues.length > 0) {
-        this.risksWithActionsCount += 1;
-      }
-    }
+    this.risksWithActionsCount = this.risks.reduce((ct: number, risk: Risk) =>
+      ct += risk.relatedAdaptiveValues.length ? 1 : 0, 0);
   }
-
 }
