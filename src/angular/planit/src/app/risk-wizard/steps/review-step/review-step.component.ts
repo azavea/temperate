1507dadf3d0f0684b7f9dbcbbedadff2daabe0f8
CloudNiner <andrew@cloudniner.co>
2018-01-11 13:35:56 -0500

    Fix issue where risk data sometimes doesn't update
    It's unclear why this happens in some cases.
    The risk object should keep the same object ref
    throughout the entire form, but doesn't sometimes.
    Either way, we now update the risk object whenever
    it changes via the session data subscription.

--- src/angular/planit/src/app/risk-wizard/steps/review-step/review-step.component.ts
+++ src/angular/planit/src/app/risk-wizard/steps/review-step/review-step.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -10,13 +10,15 @@ import { Risk,
 import { RiskStepKey } from '../../risk-step-key';
 
 import { WizardSessionService } from '../../../core/services/wizard-session.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-risk-step-review',
   templateUrl: 'review-step.component.html'
 })
 
-export class ReviewStepComponent extends WizardStepComponent<Risk, any> implements OnInit {
+export class ReviewStepComponent extends WizardStepComponent<Risk, any>
+                                 implements OnDestroy, OnInit {
 
   public navigationSymbol = '5';
   public title = 'Review';
@@ -29,6 +31,8 @@ export class ReviewStepComponent extends WizardStepComponent<Risk, any> implemen
   public chanceOptions = OrgRiskRelativeChanceOptions;
   public impactOptions = OrgRiskRelativeImpactOptions;
 
+  private sessionSubscription: Subscription;
+
   constructor(private router: Router,
               protected session: WizardSessionService<Risk>) {
     super(session);
@@ -37,6 +41,11 @@ export class ReviewStepComponent extends WizardStepComponent<Risk, any> implemen
   ngOnInit() {
     super.ngOnInit();
     this.risk = this.session.getData();
+    this.sessionSubscription = this.session.data.subscribe(r => this.risk = r);
+  }
+
+  ngOnDestroy() {
+    this.sessionSubscription.unsubscribe();
   }
 
   getFormModel() {
