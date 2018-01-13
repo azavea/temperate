2d4c13ec98127dd2c84427b075ce67cb99cc880c
Reed Martz <rmartz@azavea.com>
2018-01-13 09:46:08 -0500

    Add FactoryBoy to streamline complex testing

---
+++ src/django/planit_data/tests/factories.py
@@ -0,0 +1,78 @@
+import factory
+
+from django.contrib.gis.geos import MultiPolygon, Polygon
+
+from users.tests.factories import OrganizationFactory
+from planit_data.models import (
+    CommunitySystem,
+    Concern,
+    GeoRegion,
+    Indicator,
+    OrganizationRisk,
+    WeatherEvent,
+    WeatherEventRank
+)
+
+
+class CommunitySystemFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = CommunitySystem
+
+    name = 'Test Community System'
+
+
+class IndicatorFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Indicator
+
+    name = 'test_indicator'
+
+
+class ConcernFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Concern
+
+    indicator = factory.SubFactory(IndicatorFactory)
+
+
+class GeoRegionFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = GeoRegion
+
+    @classmethod
+    def _adjust_kwargs(cls, **kwargs):
+        # Wrap boundaries in GeoDjango types
+        bounds = kwargs.pop('bounds', None)
+        if bounds:
+            kwargs['geom'] = MultiPolygon(Polygon(bounds))
+
+        return kwargs
+
+    name = 'Test GeoRegion'
+    # Default georegion is a square bounding from 1,1 to 4,4 to exclude 0,0
+    geom = MultiPolygon(Polygon([[1, 1], [1, 4], [4, 4], [4, 1], [1, 1]]))
+
+
+class WeatherEventFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = WeatherEvent
+
+    name = factory.Sequence(lambda n: 'Test WeatherEvent {}'.format(n))
+
+
+class WeatherEventRankFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = WeatherEventRank
+
+    georegion = factory.SubFactory(GeoRegionFactory)
+    weather_event = factory.SubFactory(WeatherEventFactory)
+    order = factory.Sequence(int)
+
+
+class OrganizationRiskFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = OrganizationRisk
+
+    organization = factory.SubFactory(OrganizationFactory)
+    weather_event = factory.SubFactory(WeatherEventFactory)
+    community_system = factory.SubFactory(CommunitySystemFactory)
--- src/django/planit_data/tests/test_views.py
+++ src/django/planit_data/tests/test_views.py
@@ -1,29 +1,24 @@
 from unittest import mock
 
-from django.contrib.auth import get_user_model
-from django.contrib.gis.geos import MultiPolygon, Point, Polygon
+from django.contrib.gis.geos import Point
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 
-from planit_data.models import Concern, GeoRegion, Indicator, WeatherEvent, WeatherEventRank
 from planit_data.views import WeatherEventRankView
-from users.models import PlanItLocation, PlanItOrganization
+from planit_data.tests.factories import (
+    ConcernFactory,
+    GeoRegionFactory,
+    OrganizationRiskFactory,
+    WeatherEventRankFactory
+)
+from users.tests.factories import UserFactory, LocationFactory
 
 
 class ConcernViewSetTestCase(APITestCase):
     def setUp(self):
-        user_class = get_user_model()
-        self.user = user_class.objects.create_user('user', 'user@example.com',
-                                                   'password')
-
+        self.user = UserFactory()
         self.client.force_authenticate(user=self.user)
-
-        location = PlanItLocation.objects.create(api_city_id=14)
-        org = PlanItOrganization.objects.create(name='Test', location=location)
-        self.user.organizations.add(org)
-        self.user.primary_organization = org
-        self.user.save()
 
     def test_concern_list_empty(self):
         url = reverse('concern-list')
@@ -34,25 +29,29 @@ class ConcernViewSetTestCase(APITestCase):
 
     @mock.patch('planit_data.models.Concern.calculate')
     def test_concern_list_nonempty(self, calculate_mock):
-        indicator = Indicator.objects.create(name='Foobar')
         calculate_mock.return_value = {
             'value': 5.3,
             'units': 'farthing',
             'tagline': 'more'
         }
-        concern = Concern.objects.create(indicator=indicator,
-                                         tagline_positive='more',
-                                         tagline_negative='less',
-                                         is_relative=True)
+        concern = ConcernFactory(
+            tagline_positive='more',
+            tagline_negative='less',
+            is_relative=True)
 
         url = reverse('concern-list')
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data['results']), 1)
-        self.assertDictEqual(dict(response.data['results'][0]),
-                             {'id': concern.id, 'indicator': 'Foobar', 'tagline': 'more',
-                              'isRelative': True, 'value': 5.3, 'units': 'farthing'})
+        self.assertDictEqual(
+            dict(response.data['results'][0]),
+            {'id': concern.id,
+             'indicator': concern.indicator.name,
+             'tagline': 'more',
+             'isRelative': True,
+             'value': 5.3,
+             'units': 'farthing'})
 
     def test_concern_list_nonauth(self):
         """Ensure that unauthenticated users receive a 401 Unauthorized response."""
@@ -70,18 +69,17 @@ class ConcernViewSetTestCase(APITestCase):
             'tagline': 'more'
         }
 
-        indicator = Indicator.objects.create(name='Foobar')
-        concern = Concern.objects.create(indicator=indicator,
-                                         tagline_positive='more',
-                                         tagline_negative='less',
-                                         is_relative=True)
+        concern = ConcernFactory(
+            tagline_positive='more',
+            tagline_negative='less',
+            is_relative=True)
 
         url = reverse('concern-detail', kwargs={'pk': concern.id})
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertDictEqual(response.data,
-                             {'id': concern.id, 'indicator': 'Foobar',
+                             {'id': concern.id, 'indicator': concern.indicator.name,
                               'tagline': 'more', 'isRelative': True, 'value': 5.3,
                               'units': 'miles'})
         calculate_mock.assert_called_with(self.user.primary_organization)
@@ -103,42 +101,61 @@ class ConcernViewSetTestCase(APITestCase):
 class WeatherEventRankViewTestCase(APITestCase):
 
     def setUp(self):
-        user_class = get_user_model()
-        self.user = user_class.objects.create_user('user', 'user@example.com',
-                                                   'password')
-
+        self.user = UserFactory()
         self.client.force_authenticate(user=self.user)
 
     def test_weather_event_rank_list(self):
-        location = PlanItLocation.objects.create(name='test location', point=Point(1, 1))
-        organization = PlanItOrganization.objects.create(name='test org', location=location)
-        self.user.organizations.add(organization)
-        self.user.primary_organization = organization
-        self.user.save()
+        organization = self.user.primary_organization
+        organization.location = LocationFactory(coords=(2, 2))
 
-        geom = MultiPolygon(Polygon([[0, 0], [0, 4], [4, 4], [4, 0], [0, 0]]))
-        georegion = GeoRegion.objects.create(name='test geom', geom=geom)
+        # Create a georegion centered around our location's coordinates
+        georegion = GeoRegionFactory(bounds=[[1, 1], [1, 3], [3, 3], [3, 1], [1, 1]])
 
-        rrr1 = WeatherEventRank.objects.create(
-            georegion=georegion,
-            weather_event=WeatherEvent.objects.create(
-                name='Heat Events'
-            ),
-            order=1
-        )
-        WeatherEventRank.objects.create(
-            georegion=georegion,
-            weather_event=WeatherEvent.objects.create(
-                name='Hurricanes'
-            ),
-            order=2
-        )
+        # Create a WeatherEventRank that will be associated with our organization
+        weathereventank = WeatherEventRankFactory(georegion=georegion)
+        organization.weather_events.add(weathereventank)
 
-        # Add rrr1 but not rrr2
-        organization.weather_events.add(rrr1)
+        # Create additional WeatherEventRanks for the same georegion that are not associated
+        WeatherEventRankFactory.create_batch(2, georegion=georegion)
 
         url = reverse('weather-event-rank-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        serializer = WeatherEventRankView.serializer_class([rrr1], many=True)
+        serializer = WeatherEventRankView.serializer_class([weathereventank], many=True)
         self.assertEqual(response.json(), serializer.data)
+
+
+class OrganizationRiskTestCase(APITestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.client.force_authenticate(user=self.user)
+
+    def test_list_organization_risks(self):
+        org_risk = OrganizationRiskFactory(organization=self.user.primary_organization)
+
+        url = reverse('organizationrisk-list')
+        response = self.client.get(url)
+
+        self.assertEqual(response.json(), [{
+            'adaptiveCapacity': '',
+            'adaptiveCapacityDescription': '',
+            'communitySystem': {
+                'id': org_risk.community_system.id,
+                'name': org_risk.community_system.name
+            },
+            'frequency': '',
+            'id': str(org_risk.id),
+            'impactDescription': '',
+            'impactMagnitude': '',
+            'intensity': '',
+            'probability': '',
+            'relatedAdaptiveValues': [],
+            'weatherEvent': {
+                'coastalOnly': False,
+                'concern': None,
+                'displayClass': '',
+                'id': org_risk.weather_event.id,
+                'indicators': [],
+                'name': org_risk.weather_event.name
+            }}])
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
