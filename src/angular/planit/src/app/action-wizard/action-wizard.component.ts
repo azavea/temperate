b11fb5f89139fd07521517b92cbda869f45721f5
Reed Martz <rmartz@azavea.com>
2018-02-02 11:03:55 -0500

    View jumps to Review step

--- src/angular/planit/src/app/action-wizard/action-wizard.component.ts
+++ src/angular/planit/src/app/action-wizard/action-wizard.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { AfterViewInit, Component, HostListener, Input, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
 
 // Import from root doesn't seem to pickup types, so import directly from file
@@ -37,6 +37,8 @@ export class ActionWizardComponent implements AfterViewInit, OnInit {
 
   @Input() action: Action;
 
+  startingStep: number = 0;
+
   constructor(private session: WizardSessionService<Action>,
               private actionService: ActionService,
               private riskService: RiskService,
@@ -51,6 +53,11 @@ export class ActionWizardComponent implements AfterViewInit, OnInit {
       const riskId: string = this.route.snapshot.paramMap.get('riskid');
       this.action.risk = riskId;
     }
+
+    this.route.queryParams.take(1).subscribe((params: Params) => {
+      // Jump to the requested step, but default to the front
+      this.startingStep = params['step'] || 0;
+    });
 
     this.session.setData(this.action);
   }
