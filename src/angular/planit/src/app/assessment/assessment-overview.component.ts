f296e50001b0515ae2de66955805f3ec3ca8e5ec
Reed Martz <rmartz@azavea.com>
2018-02-02 11:22:58 -0500

    Fix Take Action button

--- src/angular/planit/src/app/assessment/assessment-overview.component.ts
+++ src/angular/planit/src/app/assessment/assessment-overview.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 
+import { ActionService } from '../core/services/action.service';
 import { RiskService } from '../core/services/risk.service';
-import { Risk } from '../shared';
+import { Action, Risk } from '../shared';
 
 @Component({
   selector: 'va-overview',
@@ -12,7 +13,9 @@ export class AssessmentOverviewComponent implements OnInit {
   public risks: Risk[];
 
   constructor (private riskService: RiskService,
-               private route: ActivatedRoute) {}
+               private actionService: ActionService,
+               private route: ActivatedRoute,
+               private router: Router) {}
 
   ngOnInit() {
     this.route.queryParamMap.switchMap((params: ParamMap) => {
@@ -24,5 +27,20 @@ export class AssessmentOverviewComponent implements OnInit {
     this.riskService.delete(risk).subscribe(() => {
       this.risks = this.risks.filter(r => r.id !== risk.id);
     });
+  }
+
+  takeAction(risk: Risk) {
+    if (risk.action && risk.action.id) {
+      this.router.navigate(['actions/action/', risk.action.id]);
+    } else {
+      if (!risk.action) {
+        risk.action = new Action({
+          risk: risk.id
+        });
+      }
+      this.actionService.create(risk.action).subscribe(a => {
+        this.router.navigate(['actions/action/', a.id]);
+      });
+    }
   }
 }
