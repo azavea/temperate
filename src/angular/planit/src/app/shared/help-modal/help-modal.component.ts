129ad93956033904cbd1397cfea77acb6fa3db8b
Jenny Fung <jfung@azavea.com>
2018-02-13 11:47:13 -0500

    Replace help form with Salesforce web-to-case

--- src/angular/planit/src/app/shared/help-modal/help-modal.component.ts
+++ src/angular/planit/src/app/shared/help-modal/help-modal.component.ts
@@ -1,17 +1,32 @@
-import { Component, TemplateRef } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+
+import { UserService } from '../../core/services/user.service';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-help-modal',
   templateUrl: './help-modal.component.html',
   styleUrls: ['./help-modal.component.scss']
 })
-export class HelpModalComponent {
+export class HelpModalComponent implements OnInit {
   public modalRef: BsModalRef;
-  constructor(private modalService: BsModalService) {}
+  public user: User;
+
+  constructor(private modalService: BsModalService,
+              private userService: UserService,
+              private router: Router) {}
+
+  ngOnInit() {
+    this.userService.current().subscribe(u => this.user = u);
+  }
 
   public openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template, {animated: false});
   }
+
 }
