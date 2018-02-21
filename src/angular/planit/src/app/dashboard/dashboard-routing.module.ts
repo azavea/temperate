1490206c10f3d94a324724f4f6651a1a8d8e7d83
CloudNiner <andrew@cloudniner.co>
2018-02-21 11:08:38 -0500

    Use shared back button on city profile page

--- src/angular/planit/src/app/dashboard/dashboard-routing.module.ts
+++ src/angular/planit/src/app/dashboard/dashboard-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PlanAuthGuard } from '../core/services/plan-auth-guard.service';
+import { PreviousRouteGuard } from '../core/services/previous-route-guard.service';
 import { CityProfileComponent } from './city-profile/city-profile.component';
 import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent, canActivate: [PlanAuthGuard] },
-  { path: 'city-profile', component: CityProfileComponent, canActivate: [PlanAuthGuard] }
+  {
+    path: 'city-profile',
+    component: CityProfileComponent,
+    canActivate: [PlanAuthGuard, PreviousRouteGuard]
+  }
 ];
 
 @NgModule({
