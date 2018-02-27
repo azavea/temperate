8c05f56002f195864aa86054b5f13190a40cde05
CloudNiner <andrew@cloudniner.co>
2018-02-27 13:14:24 -0500

    Use new UserService observables in HelpModal

--- src/angular/planit/src/app/shared/help-modal/help-modal.component.ts
+++ src/angular/planit/src/app/shared/help-modal/help-modal.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Component, OnDestroy, OnInit, TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
+
+import { Subscription } from 'rxjs/Rx';
+
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
@@ -11,10 +14,13 @@ import { User } from '../models/user.model';
   selector: 'app-help-modal',
   templateUrl: './help-modal.component.html'
 })
-export class HelpModalComponent implements OnInit {
+export class HelpModalComponent implements OnInit, OnDestroy {
+  public loginSubscription: Subscription;
+  public logoutSubscription: Subscription;
   public modalRef: BsModalRef;
-  public user: User;
+  public user?: User;
   public url: string;
+  public userSubscription: Subscription;
 
   constructor(private modalService: BsModalService,
               public router: Router,
@@ -22,13 +28,31 @@ export class HelpModalComponent implements OnInit {
               private userService: UserService) {}
 
   ngOnInit() {
-    if (this.authService.isAuthenticated()) {
-      this.userService.current().subscribe(u => this.user = u);
-    }
     this.url = document.location.href;
+
+    this.getCurrentUser();
+    this.loginSubscription = this.authService.loggedIn.subscribe(() => this.getCurrentUser());
+    this.logoutSubscription = this.authService.loggedOut.subscribe(() => this.user = undefined);
+    this.userSubscription = this.userService.currentUser.subscribe(user => this.user = user);
+  }
+
+  ngOnDestroy() {
+    this.loginSubscription.unsubscribe();
+    this.logoutSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
+  }
+
+  public isButtonVisible() {
+    return this.user && this.router.url !== '/?ref=footer' && this.router.url !== '/';
   }
 
   public openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template, {animated: false});
+  }
+
+  private getCurrentUser() {
+    if (this.authService.isAuthenticated()) {
+      this.userService.current().subscribe(user => this.user = user);
+    }
   }
 }
