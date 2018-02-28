91190b545ca60a105591d4db5da77235eb798b04
Casey Cesari <ccesari@azavea.com>
2018-02-28 12:55:58 -0500

    Add initial code for "basic" subscription plan
    There may be more code required to fully implement the "basic"
    subscription plan, but this stub is enough to implement and test
    restricting the help modal to certain plans.
    
    Refs #531

--- src/angular/planit/src/app/shared/models/org-subscription.model.ts
+++ src/angular/planit/src/app/shared/models/org-subscription.model.ts
@@ -2,9 +2,11 @@
 export enum OrgSubscription {
   FreeTrial = 'free_trial',
   Custom = 'custom',
+  Basic = 'basic',
 }
 
 export const OrgSubscriptionOptions = new Map<OrgSubscription, string>([
   [OrgSubscription.FreeTrial, 'Free Trial'],
-  [OrgSubscription.Custom, 'Custom']
+  [OrgSubscription.Custom, 'Custom'],
+  [OrgSubscription.Basic, 'Basic'],
 ]);
