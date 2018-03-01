a2253981790c48d7495429096af91cff60a06dcf
Casey Cesari <ccesari@azavea.com>
2018-03-01 14:40:47 -0500

    Hide menu links until user selects an organization
    To avoid issues with letting users access their settings page before
    their account is fully setup, the links to these pages are hidden in the
    user menu until an organization is selected by the user.
    
    Refs #676

--- src/angular/planit/src/app/shared/user-dropdown/user-dropdown.component.ts
+++ src/angular/planit/src/app/shared/user-dropdown/user-dropdown.component.ts
@@ -1,22 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Subscription } from 'rxjs/Rx';
 
 import { AuthService } from '../../core/services/auth.service';
 import { UserService } from '../../core/services/user.service';
-import { OrgSubscriptionOptions, User } from '../../shared';
+import { OrgSubscriptionOptions, Organization, User } from '../../shared';
 
 @Component({
   selector: 'app-user-dropdown',
   templateUrl: './user-dropdown.component.html'
 })
 
-export class UserDropdownComponent implements OnInit {
+export class UserDropdownComponent implements OnDestroy, OnInit {
   public user: User;
+  public organization: Organization;
   public subscriptionOptions = OrgSubscriptionOptions;
+  public userSubscription: Subscription;
 
   constructor(private authService: AuthService, private userService: UserService) {}
 
   ngOnInit() {
-    this.userService.current().subscribe(user => this.user = user);
+    this.userSubscription = this.userService.currentUser.subscribe(user => {
+      this.user = user;
+      this.organization = user.primary_organization;
+    });
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
   }
 
   public logout() {
