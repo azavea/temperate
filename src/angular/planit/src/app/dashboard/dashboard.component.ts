0d0217d0b3358749f8d8d7018009b219b9008744
Reed Martz <rmartz@azavea.com>
2018-03-06 09:40:15 -0500

    Favor lifecycle hooks over setTimeout

--- src/angular/planit/src/app/dashboard/dashboard.component.ts
+++ src/angular/planit/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -20,7 +20,7 @@ import { environment } from '../../environments/environment';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
 
   public groupedRisks: any[];
   public selectedEventsControl = new FormControl([]);
@@ -50,11 +50,15 @@ export class DashboardComponent implements OnInit {
     });
     this.route.data.subscribe((data: {user: User}) => {
       this.organization = data.user.primary_organization;
+    });
+  }
 
+  ngAfterViewInit() {
+    this.userService.current().subscribe(user => {
       const shownWarning = this.cache.get(CacheService.APP_DASHBOARD_TRIALWARNING);
       if (!shownWarning) {
-        this.trialDaysRemaining = this.organization.trialDaysRemaining();
-        if (this.organization.isFreeTrial() && this.trialDaysRemaining <= 3) {
+        this.trialDaysRemaining = user.primary_organization.trialDaysRemaining();
+        if (user.primary_organization.isFreeTrial() && this.trialDaysRemaining <= 3) {
           this.openModal(this.trialWarningModal);
           this.cache.set(CacheService.APP_DASHBOARD_TRIALWARNING, true);
         }
@@ -75,9 +79,7 @@ export class DashboardComponent implements OnInit {
   }
 
   openModal(modal: ModalTemplateComponent) {
-    setTimeout(() => {
-      modal.open();
-    });
+    modal.open();
   }
 
   upgradeSubscription() {
