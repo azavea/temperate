cdb46db09310c873f1d54bf882998268ce2c2ac4
Kathryn Killebrew <kkillebrew@azavea.com>
2018-03-06 16:36:58 -0500

    Add Heap Analytics
    Append script to head on app load.
    Closes #631.

--- src/angular/planit/src/environments/environment.prod.ts
+++ src/angular/planit/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   // Relative URLs are used in production mode,
   // as both the Django app and the Angular app are on the same host
-  apiUrl: ''
+  apiUrl: '',
+  heapID: '2827494035',
 };
