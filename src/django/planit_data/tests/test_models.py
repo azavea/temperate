4d1c802da059d6527eb8af760cd3bbb0229d368b
Reed Martz <rmartz@azavea.com>
2018-03-13 15:32:29 -0400

    Consolidate Concern tests

--- src/django/planit_data/tests/test_models.py
+++ src/django/planit_data/tests/test_models.py
@@ -3,7 +3,7 @@ from unittest import mock
 from django.db.utils import IntegrityError
 from django.test import TestCase
 
-from planit_data.models import OrganizationWeatherEvent
+from planit_data.models import Concern, OrganizationWeatherEvent
 from planit_data.tests.factories import (
     ConcernFactory,
     OrganizationFactory,
@@ -12,6 +12,48 @@ from planit_data.tests.factories import (
 
 
 class ConcernTestCase(TestCase):
+    @mock.patch('planit_data.models.make_indicator_api_request')
+    def test_get_average_value(self, api_indicator_mock):
+        api_indicator_mock.return_value = {
+            'data': {
+                '2050': {'avg': 10},
+                '2051': {'avg': 17}
+            }}
+
+        concern = mock.Mock()
+        concern.ERA_LENGTH = 10
+
+        scenario = 'historical'
+        result = Concern.get_average_value(concern, 14, scenario, 1990, 'in')
+
+        self.assertEqual(result, 13.5)
+        api_indicator_mock.assert_called_with(concern.indicator, 14, scenario,
+                                              params={'years': [range(1990, 2000)],
+                                                      'units': 'in'})
+
+    def test_calculate(self):
+        org = OrganizationFactory()
+        concern_mock = mock.Mock(spec=Concern)
+        concern_mock.is_relative = False
+
+        # Use a lambda for get_average_value to give different values for the times it is called
+        concern_mock.get_average_value.side_effect = (lambda organization, scenario, year, units: {
+            # Give one number as the result of calculating the start value
+            concern_mock.START_SCENARIO: 7.3,
+            # Another for the end value
+            concern_mock.END_SCENARIO: 15.6
+        }.get(scenario))
+
+        result = Concern.calculate(concern_mock, org)
+
+        self.assertAlmostEqual(result['value'], 8.3)
+        concern_mock.get_average_value.assert_has_calls([
+            # Ensure that our get_average_value mock was called for start data and end data
+            mock.call(org.location.api_city_id, concern_mock.START_SCENARIO,
+                      concern_mock.START_YEAR, concern_mock.get_units()),
+            mock.call(org.location.api_city_id, concern_mock.END_SCENARIO,
+                      concern_mock.END_YEAR, concern_mock.get_units())
+        ], any_order=True)
 
     def test_concern_no_indicator_uses_static_data(self):
         concern = ConcernFactory(
