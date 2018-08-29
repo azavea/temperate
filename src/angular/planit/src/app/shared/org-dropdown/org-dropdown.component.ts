5f18b1a6a4a7fb5e7aa952e1026aa45bcf9f97ef
Kathryn Killebrew <kkillebrew@azavea.com>
2018-08-29 12:33:44 -0400

    Show confirmation dialog before switching organization

--- src/angular/planit/src/app/shared/org-dropdown/org-dropdown.component.ts
+++ src/angular/planit/src/app/shared/org-dropdown/org-dropdown.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { Subscription } from 'rxjs/Rx';
 import { ToastrService } from 'ngx-toastr';
 
 import { UserService } from '../../core/services/user.service';
 import { OrgSubscriptionOptions, Organization, User } from '../../shared';
+import {
+  ConfirmationModalComponent
+} from '../../shared/confirmation-modal/confirmation-modal.component';
 
 @Component({
   selector: 'app-org-dropdown',
@@ -12,6 +15,8 @@ import { OrgSubscriptionOptions, Organization, User } from '../../shared';
 })
 
 export class OrgDropdownComponent implements OnInit {
+  @ViewChild('confirmOrgChangeModal') confirmOrgChangeModal: ConfirmationModalComponent;
+
   public user: User;
   public primaryOrganization: Organization;
   public organizations: string[];
@@ -32,10 +37,30 @@ export class OrgDropdownComponent implements OnInit {
   }
 
   private setOrganization(organization: string): void {
-    const newOrg: string = this.organizations ?
-      this.organizations.find(org => { return org === organization; }) || '' : '';
-    this.save(this.userService.updatePrimaryOrg(this.user, newOrg),
-              'Changed primary organization');
+    // Show confirmation dialog before switching
+    const createNewOrg: boolean = !organization || organization.length === 0;
+    var tagline = createNewOrg ? 'Are you sure you want to create a new organization?' :
+        `Are you sure you want to switch to organization ${organization}?`;
+    tagline += ' Any unsaved changes on the current page will be lost.';
+
+    this.confirmOrgChangeModal.confirm({
+      title: createNewOrg ? 'Create new organization' : 'Switch organization',
+      tagline: tagline,
+      confirmButtonClass: 'button-primary',
+      confirmText: createNewOrg ? 'Create' : 'Switch'
+    }).onErrorResumeNext().switchMap(() => {
+      const newOrg: string = this.organizations ?
+        this.organizations.find(org => { return org === organization; }) || '' : '';
+      return this.userService.updatePrimaryOrg(this.user, newOrg);
+    }).subscribe(() => {
+      if (!createNewOrg) {
+        this.toastr.success('Changed primary organization');
+      }
+      this.userService.invalidate();
+      window.location.reload();
+    }, () => {
+      this.toastr.error('Something went wrong, please try again.');
+    });
   }
 
   private save(observable, success) {
