0e04bb3dd385f515e72cf38a86a1906f913d1b50
Rocky Breslow <rbreslow@azavea.com>
2019-07-26 12:49:39 -0400

    Refactor sbt-lighter S3 destinations (#1256)
    - Add `${environment}-${aws_region}-climate-planit-artifacts` bucket
    - Add `sparkS3PutObjectDecorator` for object encryption
    - Refactor `sparkS3JarFolder` and `sparkS3LogUri`

--- deployment/terraform/storage.tf
+++ deployment/terraform/storage.tf
@@ -2,3 +2,8 @@ resource "aws_s3_bucket" "logs" {
   bucket = "${lower(var.environment)}-${var.aws_region}-climate-planit-logs"
   acl    = "private"
 }
+
+resource "aws_s3_bucket" "artifacts" {
+  bucket = "${lower(var.environment)}-${var.aws_region}-climate-planit-artifacts"
+  acl    = "private"
+}
