0dd6efd9be6458104bf007427e58a8bdc7ea67a8
Michael Maurizi <mmaurizi@azavea.com>
2019-08-06 20:19:56 -0400

    Add indicator metadata endpoint

--- src/area-indicators/api/src/main/scala/io/temperate/api/DataService.scala
+++ src/area-indicators/api/src/main/scala/io/temperate/api/DataService.scala
@@ -2,6 +2,7 @@ package io.temperate.api
 
 import cats.effect._
 import com.typesafe.scalalogging.LazyLogging
+import io.circe.Encoder
 import io.circe.syntax._
 import io.temperate.datamodel._
 import org.http4s._
@@ -10,18 +11,26 @@ import org.http4s.dsl.Http4sDsl
 
 object DataService extends Http4sDsl[IO] with LazyLogging {
 
+  private def asSortedJson[T: Ordering](options: Set[T])(implicit encoder: Encoder[T]) = {
+    options.toList.sorted.asJson
+  }
+
   val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
     case GET -> Root / "climate-model" / ClimateModel(model) =>
       Ok(model.asJson)
     case GET -> Root / "climate-model" =>
-      Ok(ClimateModel.options.toList.sorted.asJson)
+      Ok(asSortedJson(ClimateModel.options))
     case GET -> Root / "dataset" / Dataset(dataset) =>
       Ok(dataset.asJson)
     case GET -> Root / "dataset" =>
-      Ok(Dataset.options.seq.toList.sorted.asJson)
+      Ok(asSortedJson(Dataset.options))
+    case GET -> Root / "indicator" / Indicator(indicator) =>
+      Ok(indicator.asJson)
+    case GET -> Root / "indicator" =>
+      Ok(asSortedJson(Indicator.options))
     case GET -> Root / "scenario" / Scenario(scenario) =>
       Ok(scenario.asJson)
     case GET -> Root / "scenario" =>
-      Ok(Scenario.options.seq.toList.sorted.asJson)
+      Ok(asSortedJson(Scenario.options))
   }
 }
