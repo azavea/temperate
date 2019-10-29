e977d17bfd2f9861d3d03d126c6988b7a07931e7
Michael Maurizi <mmaurizi@azavea.com>
2019-10-29 16:06:02 -0400

    Add ranking utility function to ImpactService

--- src/angular/planit/src/app/core/constants/cache.ts
+++ src/angular/planit/src/app/core/constants/cache.ts
@@ -2,3 +2,4 @@
 export const CORE_USERSERVICE_CURRENT = 'core.userservice.current';
 export const CORE_ACTIONTYPESERVICE_LIST = 'core.actiontypeservice.list';
 export const CORE_WEATHEREVENTSERVICE_LIST = 'core.weathereventservice.list';
+export const CORE_IMPACTSERVICE_LIST = 'core.impactservice.list';
